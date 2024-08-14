15f640516fec59116e69625ea494c061ae112cfc
Hemmaps <hemmapssivagnanam@gmail.com>
2024-08-14 12:19:27 +0530

    ch

--- back/index.js
+++ back/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import dbConnect from "./dbConfig/conn.js"
 import router from "./routes/foodRoutes.js";
 import cors from "cors";
-const port = 8000;
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT||8000;
+
 const app = express();
 app.use(cors())
 app.use(express.json())
